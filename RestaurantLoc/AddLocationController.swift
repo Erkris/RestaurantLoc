f738c880f3b8c91d1afac2a5772007a72cc3d87c
Erkris <p.rubier@gmail.com>
2015-04-02 15:04:43 +0200

    Add Table View Controller Add Map View Controller with all constraints

---
+++ RestaurantLoc/AddLocationController.swift
@@ -0,0 +1,40 @@
+//
+//  AddLocationController.swift
+//  RestaurantLoc
+//
+//  Created by iem on 02/04/2015.
+//  Copyright (c) 2015 iem. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class AddLocationController: UIViewController {
+
+    @IBAction func CancelButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
